4bbd00cbd2b9647beef2d10b0c76899fa7762b9d
Carlo <carlo@dev.com>
2026-03-28 15:04:37 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next'
 
 
-const repo = 'EnglishAppForDev';
 const nextConfig: NextConfig = {
-  // Configuração para GitHub Pages
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
   output: 'export',
   images: {
     unoptimized: true,
